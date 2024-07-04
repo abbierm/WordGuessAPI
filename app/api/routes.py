65346dba86518ebf4954ed404ff1929628a7a55c
Abbie <armiller1003@gmail.com>
2024-07-04 13:04:02 -0500

    removed  api route to create account and added tests for api lookupup routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -93,25 +93,4 @@ def lookup_user(username: str):
         solvers_payload["solvers"][solver.name] = solver.to_dict()
     return solvers_payload
 
-
-@bp.route('/create_account', methods=["POST"])
-def create_account():
-    data = request.get_json()
-
-    # Validate info before adding new user
-    if 'username' not in data or 'email' not in data or 'password' not in data:
-        return bad_request('Must include username, email, and password fields')
-    if User.check_duplicate_username(data['username']) == True:
-        return bad_request('Username already in use by another user, please try again with a different username')
-    if User.check_duplicate_email(data['email']) == True:
-        return bad_request('Email already associated with another account.')
-    
-    # Adds new user to the database
-    new_user = User(username=data['username'], email=data['email'])
-    new_user.set_password(data['password'])
-    db.session.add(new_user)
-    db.session.commit()
-    return new_user.to_dict()
-
-    
 # TODO: Add lookup for ALL the games data
--- tests/functional/test_api_lookups.py
+++ tests/functional/test_api_lookups.py
@@ -13,7 +13,6 @@ def test_db_connection(init_database):
     """
     assert current_app.config["SQLALCHEMY_DATABASE_URI"]  == 'sqlite://'
 
-
 def test_lookup_solver(test_client, api_lookups):
     """
     GIVEN a flask app configured for testing
@@ -32,11 +31,37 @@ def test_lookup_solver(test_client, api_lookups):
     assert data["avg_won"] == 75
     assert data["max_streak"] == 6
 
-# TODO: test invalid solver lookup
+def test_invalid_solver_lookup(test_client, api_lookups):
+    """
+    GIVEN a flask app configured for testing
+    WHEN '/api/lookup_solver/not_real' is requested (GET)
+    THEN check if the response is correct
+    """
+    response = test_client.get('api/lookup_solver/not_real')
+    assert response.status_code == 400
+    assert b'Unable to find solver with the name not_real' in response.data
 
-# TODO: test lookup user solver
+def test_lookup_users_solvers(test_client, api_lookups):
+    """
+    GIVEN a flask app configured for testing
+    WHEN 'api/lookup_user_solvers/user5' is requested (GET)
+    THEN check if the response is correct
+    """
+    response = test_client.get('api/lookup_user_solvers/user5')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["solvers"]["solver51"]["name"] == "solver51" 
 
-# TODO: test invalid user-solver combo
 
-# TODO: test create account
+def test_invalid_user_solver_combo(test_client, api_lookups):
+    """
+    GIVEN a flask app configured for testing
+    WHEN 'api/lookup_user_solver/notReal' is requested
+    THEN check if the response is correct
+    """
+    response = test_client.get('api/lookup_user_solvers/notReal')
+    assert response.status_code == 400
+    assert b'Unable to find a user with the username: notReal'\
+          in response.data
+
 
