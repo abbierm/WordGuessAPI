e4ef77d327b822fb6f74975fa8fc593f8e956fc8
Abbie Miller <armiller1003@gmail.com>
2024-08-25 16:28:52 -0500

    changed landing page from an image to a gif

--- app/static/code/main.js
+++ app/static/code/main.js
@@ -2,7 +2,7 @@ function darkMode() {
     var body = document.body;
     body.classList.toggle("dark-mode");
     if (body.classList.contains("dark-mode")) {
-      document.getElementById("codeColor").href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/darkmoss.css";
+      document.getElementById("codeColor").href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.css";
     } else {
       document.getElementById("codeColor").href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/atelier-cave-light.min.css";
     }
